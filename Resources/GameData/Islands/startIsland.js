7a032995c631a2d7e64fc537e952007f204c4556
Bolto720 <bolto720@gmail.com>
2019-09-29 17:38:54 +0100

    Changed start island starting pos.

--- Resources/GameData/Islands/startIsland.js
+++ Resources/GameData/Islands/startIsland.js
@@ -1,8 +1,8 @@
 exports.name = "Start Island";
 exports.id = "startIsland";
 
-exports.start_x = 312;
-exports.start_y = 312;
+exports.start_x = 12000;
+exports.start_y = 12000;
 
 exports.clients = [];
 exports.creatures = [];
